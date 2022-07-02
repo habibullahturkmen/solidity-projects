e17e1c124afd23dd7c44f64d02eea56c5be11e1a
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-07-02 16:40:57 +0430

    fund-me-fcc: initial commit

---
+++ fund-me-fcc/FundMe.sol
@@ -0,0 +1,20 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.8;
+
+contract FundMe {
+    
+    function fund() {
+        // want to be able to set a minimum fund amount in USD
+    }
+
+    // function withdraw() {
+
+    // }
+
+
+}
